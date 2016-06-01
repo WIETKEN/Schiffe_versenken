f606b6c25bae1d84e328747955e63b02fa796c27
cwiegers <christian.wiegers@gmail.com>
2016-06-01 13:43:11 +0200

    FrameHolder

---
+++ src/Flugzeugtraeger.java
@@ -0,0 +1,7 @@
+/**
+ * Created by cwiegers on 01.06.2016.
+ */
+public class Flugzeugtraeger {
+    int laenge = 5;
+    Flugzeugtraeger();
+}
---
+++ src/UBoot.java
@@ -0,0 +1,7 @@
+/**
+ * Created by cwiegers on 01.06.2016.
+ */
+public class UBoot extends Schiffstyp {
+ int laenger = 3;
+    UBoot();
+}
