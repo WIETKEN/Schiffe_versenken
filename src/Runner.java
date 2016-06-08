0cdc67a8a3045a447b5acb169000583639ab7c6a
Denis Oetken <denisoetken@hotmail.de>
2016-06-08 10:55:13 +0200

    Runner

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,10 @@
 /**
  * Created by doetken on 24.05.2016.
  */
-public class HelloWorld {
+public class Runner {
     public static void main(String[] args) {
-       new Audioabspielen().musik();
-       new FrameHolder();
+        new Audioabspielen().musik();
+        new FrameHolder();
     }
 
 
